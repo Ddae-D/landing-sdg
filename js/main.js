9959f348699eff101819d089209925fdccd63508
Ddae-D <87243869+Ddae-D@users.noreply.github.com>
2021-07-12 15:44:46 -0500

    Sincronizacion de index con index-fancybox

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#content-main a[rel=gallery]").fancybox({
+        overlayColor: "#2c3e50",
+        opacity: true,
+        modal: false,
+        padding: 2,
+        margin: 45,
+        overlayOpacity: .9,
+        transitionIn: "elastic",
+        transitionOut: "easeOutElastic",
+        cyclic: true,
+        titlePosition: "over",
+        enableEscapeButton: false,
+        speedIn: 500,
+        speedOut: 500,
+    });
+});
