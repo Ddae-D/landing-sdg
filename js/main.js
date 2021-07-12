2f1ec07025fa79b921297a285885af8c09381a53
Ddae-D <87243869+Ddae-D@users.noreply.github.com>
2021-07-12 16:57:57 -0500

    carga y ajuste de plugin validate

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+window.jQuery || document.write('<script src="js/jquery-3.6.0.js"><\/script>');
 $(document).ready(function() {
     $("#content-main a[rel=gallery]").fancybox({
         overlayColor: "#2c3e50",
@@ -14,6 +15,59 @@ $(document).ready(function() {
         speedIn: 500,
         speedOut: 500,
     });
-
+    /*
+     **********************************************
+     */
 });
-window.jQuery || document.write('<script src="js/jquery-3.6.0.js"><\/script>')
+$(document).ready(function() {
+    $("#content-main #cont-form #formuler #user").focus();
+    $("#formuler").validate({
+        rules: {
+            user: {
+                required: true,
+            },
+            pass: {
+                required: true,
+                rangelength: [8, 16]
+            },
+            conf: {
+                equalTo: "#pass"
+            }
+        },
+        messages: {
+            user: {
+                required: "campo user requerido"
+            },
+            pass: {
+                required: "password obligatoria",
+                rangelength: "Rango de caracteres invalidos"
+            },
+            conf: {
+                equalTo: "No coinciden las contraseñas"
+            }
+        },
+        errorPlacement: function(error, element) {
+            if (element.is(":radio") || element.is(":checkbox")) {
+                error.appendTo(element.parent()); //agrega al elemento padre el error
+            } else {
+                error.insertAfter(element);
+            }
+        }
+    });
+    $("#formuler").submit(completedFormuler);
+});
+
+function completedFormuler() {
+    var con = 0;
+    var user = $("#user").val();
+    var arrayPass = new Array();
+    arrayPass.push(user);
+    $(":password").each(function() {
+        arrayPass.push($(this).val());
+    });
+    if (arrayPass[0] != "" && arrayPass[1] != "" && arrayPass[2] != "") {
+        if (arrayPass[1] == arrayPass[2]) {
+            alert("user: " + arrayPass[0] + "\npassword: " + md5(arrayPass[1]));
+        }
+    }
+}
